44dddfcc3b0c2048efabd55d81e237b28672184f
idaboren <idaboren@gmail.com>
2022-11-21 15:11:34 +0100

    ++

--- SQLÖvningarIda/Övningsuppgifter2.sql
+++ SQLÖvningarIda/Övningsuppgifter2.sql
@@ -3,15 +3,22 @@ use everyloop;
 -- 1. Företagets totala produktkatalog består av 77 unika produkter. 
 --Om vi kollar bland våra ordrar, hur stor andel av dessa produkter 
 --har vi någon gång leverarat till London?
--- Svar: 49/77
+-- Svar: 41/77
 
---select company.order_details.OrderId, 
---company.order_details.ProductId, 
---company.products.ProductName, 
---company.Orders.ShipCity
+--select distinct
+--company.products.ProductName
 --from company.orders
 --join company.order_details on company.order_details.OrderId = company.orders.Id
 --join company.products on company.order_details.ProductId = company.products.Id
---where company.orders.ShipCity = 'London';
+--where company.orders.ShipCity = 'London'
 --;
 
+--select * from company.orders
+-- 2. Till vilken stad har vi levererat flest unika produkter?
+-- Svar:
+--select company.orders.ShipCity
+--from company.orders
+--join company.order_details on company.order_details.OrderId = company.orders.Id
+--join company.products on company.order_details.ProductId = company.products.Id
+--order by company.orders.ShipCity
+--;
